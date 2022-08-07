ad88b01dbe1c865b7ea90d005a8dbab4e6a364ad
Nathan Ireland <nathangireland@gmail.com>
2022-08-06 23:30:05 -0500

    Added Project files

---
+++ TimeConverter/TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Nathan Ireland on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
